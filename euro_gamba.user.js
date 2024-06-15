6870f9bb46a371dbefc6bdb5ea2d703d5a432c9b
frenchcutgreenbean <fartass55@protonmail.com>
2024-06-14 20:50:12 -0400

    reward change

--- euro_gamba.user.js
+++ euro_gamba.user.js
@@ -11,13 +11,14 @@
 
 (function () {
   "use strict";
+
   const tournament = "EURO_24";
   let usernameAnchor = document.querySelector(
     ".top-nav__username--highresolution"
   );
   let username = usernameAnchor.querySelector("span").innerText.trim();
   let giftURL = `https://blutopia.cc/users/${username}/gifts`;
-  let betsData, teamsData, teamOdds;
+  let betsData, teamsData, totalWagered;
 
   function GM_xmlHttpRequest_promise(details) {
     return new Promise((resolve, reject) => {
@@ -90,81 +91,110 @@
     const teamExamples = ["esp", "Germany"];
 
     const forumMessage = `
-[center]
-[url=${mainImage}][img=350]${mainImage}[/img][/url] 
+    [center]
+    [url=${mainImage}][img=350]${mainImage}[/img][/url] 
 
-[b][size=24]${tournamentTitle}[/size][/b]
+    [b][size=24]${tournamentTitle}[/size][/b]
 
-Bet BON.
-[/center]
+    Bet BON.
+    [/center]
 
-[b][size=24]How it works:[/size][/b]
-[list]
-[*]Wager to win BON based on total of other bets & total of other bets on your prediction.
-[*]Odds are based on the assumption you are betting 100k.
-[*]Odds are locked when betting is CLOSED.
-[*]If the total pot is 1m and you were the only user to predict x team you win the whole pot. [/list]
+    [b][size=24]How it works:[/size][/b]
+    [list]
+    [*]Wager to win BON based on total of other bets & total of other bets on your prediction.
+    [*]Odds are based on the assumption you are betting 100k.
+    [*]Odds are locked when betting is CLOSED.
+    [*]If the total pot is 1m and you were the only user to predict x team you win the whole pot. 
+    [/list]
 
-[b][size=24]How to enter:[/size][/b][list]
-[*]Send me a gift with the message "${giftExample} {team}"
-Chatbox Examples:
-/gift ${username} 100000 ${giftExample} ${teamExamples[0]}
-/gift ${username} 100000 ${giftExample} ${teamExamples[1]}
-[*]Make sure your wager is between 50k and 500k.
-[/list]
+    [b][size=24]How to enter:[/size][/b][list]
+    [*]Send me a gift with the message "${giftExample} {team}"
+    Chatbox Examples:
+    /gift ${username} 100000 ${giftExample} ${teamExamples[0]}
+    /gift ${username} 100000 ${giftExample} ${teamExamples[1]}
+    [*]Make sure your wager is between 50k and 500k.
+    [/list]
 
-[b][size=24]Rules:[/size][/b]
+    [b][size=24]Rules:[/size][/b]
 
-[list]
-[*]You can only make 1 wager!
-[*]Bet limits are 50k min and 500k max.
-[*]No refunds on valid wagers.
-[/list]
+    [list]
+    [*]You can only make 1 wager!
+    [*]Bet limits are 50k min and 500k max.
+    [*]No refunds on valid wagers.
+    [/list]
+  `;
 
-    `;
-    teamOdds = calculateOdds(teamsData, betsData);
     let tableInfo = "[table]";
-    for (let teamIndex in teamOdds) {
-      let teamName = teamsData[teamIndex][0];
+    console.log(betsData, teamsData);
+    for (let teamIndex in teamsData) {
+      // Get team name and format it
+      let teamName = teamsData[teamIndex][0].toLowerCase();
       teamName = teamName[0].toUpperCase() + teamName.substring(1);
-      console.log(teamOdds[teamIndex])
-      tableInfo += `[tr][td]${teamName}[/td][td]${teamOdds[teamIndex]}[/td][/tr]`;
 
-      if (teamIndex == Object.keys(teamOdds).length - 1) {
-        const date = new Date();
-        tableInfo += `[/table]
-            Last Updated (UTC): ${date.toUTCString()}
-            `;
+      // Initialize teamWagers for current team
+      let teamWagers = 0;
+
+      // Iterate through betsData to sum wagers on current team
+      for (let user in betsData) {
+        if (betsData[user].team === parseInt(teamIndex)) {
+          teamWagers += betsData[user].wager;
+        }
       }
+
+      // Calculate odds
+      const maxPayoutRatio = totalWagered / 100000; // Assuming base wager of 100000
+      let odds = teamWagers > 0 ? maxPayoutRatio / (teamWagers / 100000) : 0;
+
+      // Log and build tableInfo string
+      console.log(odds.toFixed(2));
+      tableInfo += `[tr][td]${teamName}[/td][td]${odds.toFixed(2)}[/td][/tr]`;
     }
+
+    const date = new Date();
+    tableInfo += `[/table]
+      Last Updated (UTC): ${date.toUTCString()}
+      `;
+
     let newMessage = forumMessage + "[center]" + tableInfo + "[/center]";
     console.log(newMessage);
   }
 
   function getTourneyData() {
     const betURL = `http://127.0.0.1:5000/bets/${tournament}`;
-    const teamURL = `http://127.0.0.1:5000/teams/${tournament}`;
+
     GM_xmlHttpRequest_promise({
       method: "GET",
       url: betURL,
-    }).then((response) => {
-      betsData = JSON.parse(response.responseText);
-      console.log(betsData);
-    });
+    })
+      .then((response) => {
+        betsData = JSON.parse(response.responseText);
+        totalWagered = Object.values(betsData).reduce(
+          (acc, bet) => acc + bet.wager,
+          0
+        );
+        getTeamsData();
+      })
+      .catch((error) => {
+        console.error("Error in retrieving bets data:", error);
+      });
+  }
+
+  function getTeamsData() {
+    const teamURL = `http://127.0.0.1:5000/teams/${tournament}`;
     GM_xmlHttpRequest_promise({
       method: "GET",
       url: teamURL,
-    }).then((response) => {
-      teamsData = JSON.parse(response.responseText);
-      console.log(teamsData);
-      // Print or display odds
+    })
+      .then((response) => {
+        teamsData = JSON.parse(response.responseText);
 
-      teamOdds = calculateOdds(teamsData, betsData);
-      editForum();
-    });
+        getGifts();
+        editForum();
+      })
+      .catch((error) => {
+        console.error("Error in retrieving teams data:", error);
+      });
   }
-
-  getTourneyData();
 
   function addBet(username, wager, team) {
     if (wager > 500000 || wager < 50000) {
@@ -174,6 +204,7 @@ Chatbox Examples:
       );
       return;
     }
+
     const betURL = `http://127.0.0.1:5000/bet`;
     GM_xmlHttpRequest_promise({
       headers: {
@@ -188,35 +219,14 @@ Chatbox Examples:
         wager,
         team,
       }),
-    });
-    getTourneyData();
+    })
+      .then(() => {
+        getTourneyData();
+      })
+      .catch((error) => {
+        console.error("Error in adding bet:", error);
+      });
   }
-  getGifts();
 
-  function calculateOdds(teams, bets) {
-    let totalWagered = 0;
-    for (let user in bets) {
-      totalWagered += bets[user].wager;
-    }
-    let maxPayoutRatio = totalWagered / 100000; // Assuming base wager of 100000
-
-    let odds = {};
-    for (let teamIndex in teams) {
-      let teamWagers = 0;
-
-      for (let user in bets) {
-        if (bets[user].team === parseInt(teamIndex)) {
-          teamWagers += bets[user].wager;
-        }
-      }
-
-      if (teamWagers > 0) {
-        odds[teamIndex] = maxPayoutRatio / (teamWagers / 100000);
-      } else {
-        odds[teamIndex] = 0; // Set default odds if no wagers are placed on the team
-      }
-    }
-
-    return odds;
-  }
+  getTourneyData();
 })();
